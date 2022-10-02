2635ce66346ff0a5de24ba3119d56b303d24f131
Ernesto <ernesto92.mg@gmail.com>
2022-10-01 20:42:48 -0400

    refactor: lint-fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,7 @@ import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-  ],
+  plugins: [Vue()],
   test: {
     globals: true,
     environment: 'jsdom',
